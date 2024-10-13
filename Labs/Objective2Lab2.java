f63b38fb394eba25b248d1386f71bb0d3d7177b2
Erin Hermanson <ErinAHermanson@gmail.com>
2024-10-13 03:53:53 -0600

    Uploading Objectives 1 and 2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2 
+{
+  public static void main(String[] args) 
+  {
+    String name = "Erin Hermanson";
+    int age = 40;
+    String state = "California";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+    
+  }
+}
