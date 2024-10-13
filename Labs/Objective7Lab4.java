0901e1d12f855790ef4ac1c4fed86797e4d52957
Erin Hermanson <ErinAHermanson@gmail.com>
2024-10-13 03:57:51 -0600

    Uploading Objectives 3 through 9

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,19 @@
+public class Objective7Lab4 
+{
+
+	public static void main(String[] args) 
+	{
+		int sumTotal = 0;
+		int counter = 0;
+		
+
+		while(counter < 20)
+		{
+			counter++;
+			sumTotal = sumTotal + counter;
+			
+		}
+		
+		System.out.println(sumTotal);
+	}
+}
